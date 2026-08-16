ca72a4560ec111dc88bbfbdfba869735d03b2342
Dharshith Dev <dharshithdevkp@gmail.com>
2026-08-16 13:12:21 +0530

    Predictions

--- ml/train_model.py
+++ ml/train_model.py
@@ -1,12 +1,51 @@
 import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+
 
 df = pd.read_csv("data/kc1.csv")
 
 X = df.drop("defects", axis=1)
 y = df["defects"]
 
-print("Features:")
-print(X.shape)
+X_train, X_test, y_train, y_test = train_test_split(
+    X,
+    y,
+    test_size=0.2,
+    random_state=42,
+    stratify=y
+)
 
-print("\nTarget:")
-print(y.shape)
+print("Training features:", X_train.shape)
+print("Testing features:", X_test.shape)
+print("Training target:", y_train.shape)
+print("Testing target:", y_test.shape)
+
+model = LogisticRegression(max_iter=1000)
+
+model.fit(X_train, y_train)
+
+y_pred = model.predict(X_test)
+print(y_pred);
+
+print("\nActual:")
+print(y_test[:10].to_numpy())
+
+print("\nPredicted:")
+print(y_pred[:10])
+accuracy = accuracy_score(y_test, y_pred)
+
+print("\nAccuracy:", accuracy)
+
+print("\nPredicted distribution:")
+print(pd.Series(y_pred).value_counts())
+
+precision = precision_score(y_test, y_pred)
+recall = recall_score(y_test, y_pred)
+f1 = f1_score(y_test, y_pred)
+
+print("\nPrecision:", precision)
+print("Recall:", recall)
+print("F1 Score:", f1)
