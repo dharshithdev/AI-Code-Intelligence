a7e539889a8de12a5be787090a3a8e562c422dc6
Dharshith Dev <dharshithdevkp@gmail.com>
2026-08-17 11:22:10 +0530

    Pipeline

--- ml/train_model.py
+++ ml/train_model.py
@@ -1,16 +1,11 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import accuracy_score
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 from sklearn.metrics import confusion_matrix
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.preprocessing import StandardScaler
 from sklearn.pipeline import Pipeline
-from xgboost import XGBClassifier
-from sklearn.model_selection import GridSearchCV
-from sklearn.feature_selection import SelectFromModel
-
 
 df = pd.read_csv("data/kc1.csv")
 
@@ -30,43 +25,20 @@ X_train, X_test, y_train, y_test = train_test_split(
 #print("Training target:", y_train.shape)
 #print("Testing target:", y_test.shape)
 
-selector_model = RandomForestClassifier(
-    n_estimators=200,
-    class_weight="balanced",
-    random_state=42
-)
+model = Pipeline([
+    ("classifier", RandomForestClassifier(
+        n_estimators=200,
+        class_weight="balanced",
+        random_state=42
+    ))
+])
 
-selector_model.fit(X_train, y_train)
+model.fit(X_train, y_train)
+y_pred = model.predict(X_test)
 
-selector = SelectFromModel(
-    selector_model,
-    threshold="mean",
-    prefit=True
-)
-
-X_train_selected = selector.transform(X_train)
-X_test_selected = selector.transform(X_test)
-
-print("Original features:", X_train.shape[1])
-print("Selected features:", X_train_selected.shape[1])
-
-model = RandomForestClassifier(
-    n_estimators=200,
-    class_weight="balanced",
-    random_state=42
-)
-
-model.fit(X_train_selected, y_train)
-
-y_pred = model.predict(X_test_selected)
-
-precision = precision_score(y_test, y_pred)
-recall = recall_score(y_test, y_pred)
-f1 = f1_score(y_test, y_pred)
-
-print("\nPrecision:", precision)
-print("Recall:", recall)
-print("F1 Score:", f1)
+print("\nPrecision:", precision_score(y_test, y_pred))
+print("Recall:", recall_score(y_test, y_pred))
+print("F1 Score:", f1_score(y_test, y_pred))
 
 cm = confusion_matrix(y_test, y_pred)
 
