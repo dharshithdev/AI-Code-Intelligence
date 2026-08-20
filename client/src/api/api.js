80b73948cb281a48c5aedb5380e2ec8d13c1fd48
Dharshith Dev <dharshithdevkp@gmail.com>
2026-08-20 14:07:37 +0530

    Changed Timeout

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
     baseURL: import.meta.env.VITE_API_URL || 'http://127.0.0.1:5000',
-    timeout: 30000,
+    timeout: 50000,
     headers: {
         'Content-Type': 'multipart/form-data',
     },
